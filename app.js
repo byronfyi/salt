69d81feb4337b06e0d71961e7dbdc6136ce95d3b
Byron <byron@wearefyi.co.uk>
2022-10-21 16:55:25 +0100

    Initial commit, start styling main page

---
+++ app.js
@@ -0,0 +1,55 @@
+let scrollpos = window.scrollY;
+const header = document.querySelector(".header");
+const offsetHeight = 240;
+
+const addClassOnScroll = (className) => header.classList.add(className);
+const removeClassOnScroll = (className) => header.classList.remove(className);
+
+window.addEventListener('scroll', handleScroll);
+window.addEventListener('load', handleScroll);
+
+function handleScroll() {
+    scrollpos = window.scrollY;
+
+    if (scrollpos >= offsetHeight) {
+        addClassOnScroll('header--light');
+    } else {
+        removeClassOnScroll('header--light');
+    }
+}
+
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1.25,
+    slidesToScroll: 1,
+    draggable: true,
+    arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next'
+    },
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2.5,
+            }
+        }
+    ]
+});
+
+new Glider(document.querySelector('.whats-on'), {
+    slidesToShow: 1.25,
+    slidesToScroll: 1,
+    draggable: true,
+    arrows: {
+        prev: '.whats-on-prev',
+        next: '.whats-on-next'
+    },
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2.5,
+            }
+        }
+    ]
+});
