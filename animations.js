6140dde0632ff1be921f0ace45e666727ce20567
Byron <byron@wearefyi.co.uk>
2022-11-17 15:47:58 +0000

    Slight update to animations

--- animations.js
+++ animations.js
@@ -23,29 +23,29 @@ gsap.utils.toArray(".paralax").forEach((section, i) => {
 });
 
 gsap.from(".t1 span", {
-  stagger: 0.25,
+  stagger: 0.15,
   opacity: 0,
   y: 40,
 });
 
 gsap.from(".t2", {
   scrollTrigger: '.t2',
-  stagger: 0.25,
-  delay: 0.25,
+  stagger: 0.15,
+  delay: 0.15,
   opacity: 0,
   y: 40,
 });
 
 gsap.from(".t3-a", {
   scrollTrigger: '.t3-a',
-  delay: 0.25,
+  delay: 0.15,
   opacity: 0,
   x: -400,
 });
 
 gsap.from(".t3-b", {
   scrollTrigger: '.t3-b',
-  delay: 0.25,
+  delay: 0.15,
   opacity: 0,
   x: 400,
 });
@@ -64,15 +64,15 @@ gsap.from(".t4", {
 
 gsap.from(".t5", {
   scrollTrigger: '.t5',
-  delay: 0.25,
+  delay: 0.15,
   opacity: 0,
   x: -400,
 });
 
 gsap.from(".t6", {
   scrollTrigger: '.t6',
-  stagger: 0.25,
-  delay: 0.25,
+  stagger: 0.15,
+  delay: 0.15,
   opacity: 0,
   y: 40,
 });
