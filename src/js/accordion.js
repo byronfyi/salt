bf4d3713b4c71956e19020bccf43d74ccb770543
Byron <byron@wearefyi.co.uk>
2023-01-11 10:01:50 +0000

    Remove console.log

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -6,15 +6,13 @@ const accordion = () => {
       this.classList.toggle("is-open");
 
       let content = this.nextElementSibling;
-      console.log(content);
-
+      
       if (content.style.maxHeight) {
         //this is if the accordion is open
         content.style.maxHeight = null;
       } else {
         //if the accordion is currently closed
         content.style.maxHeight = content.scrollHeight + "px";
-        console.log(content.style.maxHeight);
       }
     };
   });
